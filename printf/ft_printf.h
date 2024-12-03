fb6e626263075a52569a529e2dcebbd504135ee1
Genís Picó Cordo <genis.pico@hotmail.com>
2024-12-03 11:19:55 +0100

    Add printf

---
+++ printf/ft_printf.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gpico-co <gpico-co@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/15 11:17:51 by gpico-co          #+#    #+#             */
+/*   Updated: 2024/10/15 11:17:53 by gpico-co         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <stdarg.h>
+# include <unistd.h>
+
+void	ft_putstr(const char *s, unsigned int *counter);
+void	ft_putnbr(int n, unsigned int *counter);
+void	ft_putunbr(unsigned int n, unsigned int *counter);
+void	ft_puthex(unsigned int hex, char uppercase, unsigned int *counter);
+void	ft_puthexp(unsigned long hex, unsigned int *counter);
+int		ft_printf(const char *str, ...);
+
+#endif
